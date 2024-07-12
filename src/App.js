30f7d7217bb78d7cb128b91445430d11d3aeebd0
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-07-12 23:24:11 +0900

    App修正

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
-
-export default function Square() {
+// Appコンポーネントの定義
+function App() {
   return (
-    <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
-    </>
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
   );
 }
+
+// Appコンポーネントを他のファイルで利用できるようにエクスポート
+export default App;
