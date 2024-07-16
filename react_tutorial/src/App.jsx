b462273b4372cdae3ccbea86ae48f45132a0311c
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-07-16 23:24:30 +0900

    Xボタンを２つ表示させる

--- react_tutorial/src/App.jsx
+++ react_tutorial/src/App.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Square() {
   return (
     <>
-      <div>
-       {
-  <button className="square">X</button>
-}
-      </div>
+      <button className="square">X</button>
+      <button className="square">X</button>
     </>
-  )
+  );
 }
-
-export default App
