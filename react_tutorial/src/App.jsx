f8ed51754eee7eb99cf77e5b45b278d1b44406fd
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-08-31 22:41:47 +0900

    巻き戻しのため履歴を保持するgame関数を追加

--- react_tutorial/src/App.jsx
+++ react_tutorial/src/App.jsx
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board () {
+function Board () {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
 
@@ -46,3 +46,17 @@ export default function Board () {
   </>
   );
 }
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+      </div>
+  );
+}
+
