911ad7bf5eae20df05bfef9986546e6664d4556d
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-09-01 21:37:36 +0900

    更新のためにhandlePlay関数を作成

--- react_tutorial/src/App.jsx
+++ react_tutorial/src/App.jsx
@@ -51,7 +51,11 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
   const currentSquares = history[history.length - 1];
-  
+
+  function handlePlay(nextSquares){
+    // TODO
+  }
+
   return (
     <div className="game">
       <div className="game-board">
