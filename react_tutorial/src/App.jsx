8f700538e51f5dc28b6afdd9db011bcab57aff7e
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-09-05 22:48:16 +0900

    handleClickに表示条件を追加

--- react_tutorial/src/App.jsx
+++ react_tutorial/src/App.jsx
@@ -13,6 +13,9 @@ function Square({ value, onSquareClick }) {
 
 function Board ({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
